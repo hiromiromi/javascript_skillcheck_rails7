6de4aab19318259891c61118ea2edc1a80908299
hiromiromi <rmhnhns1225@yahoo.ne.jp>
2024-10-16 12:32:40 +0900

    リクエスト送信

---
+++ app/javascript/post.js
@@ -0,0 +1,8 @@
+function post (){
+  const form = document.getElementById("form");
+  form.addEventListener("submit",(e) => {
+    e.preventDefault();
+  });
+};
+
+window.addEventListener('turbo:load', post);
